4db6e8d9b34e22100f9ba55dd38cc63e0837f1be
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2025-06-30 15:35:06 +0300

    Update page.tsx

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,5 +1,5 @@
 export default function CVPage() {
   return (
-    <embed src="/pdfs/mantvydas-deltuva-cv-english.pdf" type="application/pdf" className="w-screen h-screen"/>
+    // <embed src="/pdfs/mantvydas-deltuva-cv-english.pdf" type="application/pdf" className="w-screen h-screen"/>
   );
 }
